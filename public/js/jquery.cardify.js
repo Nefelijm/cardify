4c51d5afaf2fc9ae3cc7e5f8b56cbace77d4da4d
Nefelijm <sistemasnik20@gmail.com>
2018-01-30 16:18:42 -0500

    Agregando la hoja de estilos a la carpeta SRC

--- public/js/jquery.cardify.js
+++ public/js/jquery.cardify.js
@@ -2,52 +2,20 @@
 
 (function($) {
   $.fn.cardify = function() {
-    // Estilos del contenedor
-    $(this).css({
-      'align-items': 'stretch',
-      'align-content': 'flex-end',
-      'display': 'flex',
-      'flex-flow': 'row wrap',
-      'justify-content': 'center'
-    });
-
+    // Agregando estilos al contenedor
+    $(this).addClass('cardifyContainer');
     // Iterar entre imágenes
     $(this).find('img').each(function() {
       // Creando elementos
       $(this).wrap('<figure></figure>');
       $(this).parent().append($('<figcaption></figcaption>').text($(this).attr('alt')));
 
-      // Agregando estilos
-      $(this).css({
-        'width': '100%',
-        'opacity': '1',
-        'transition': 'opacity .5s'
-      });
-
-      $(this).parent().css({
-        'align-items': 'center',
-        'box-shadow': '#FBFCFA 10px 10px, #34D1BF -10px -10px, #CFDBD5 -5px 5px 30px 10px',
-        'border': '3px solid',
-        'display': 'flex',
-        'flex-flow': 'column',
-        'justify-content': 'center',
-        'margin': '2%',
-        'padding': '2.5%',
-        'position': 'relative',
-        'width': '25%'
-      });
-
-      $(this).next().css({
-        'font-size': '1.5em',
-        'line-height': '1.2',
-        'opacity': '0',
-        'padding': '5%',
-        'position': 'absolute',
-        'text-align': 'center',
-        'transition': 'opacity .5s',
-        'width': '90%'
-      });
-
+      // Agregando estilos a la imagen
+      $(this).addClass('cardifyImage');
+      // Agregando estilos al figure
+      $(this).parent().addClass('cardifyFigure');
+      // Agregando estilos al Figcaption
+      $(this).next().addClass('cardifyFigcaption');
       // Agregando efecto hover
       $(this).parent().hover(function() {
         $(this).children(':first').css('opacity', '0');
