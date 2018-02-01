7b5ca710810675d04ebb179f387dc00ff171dcd3
Nefelijm <sistemasnik20@gmail.com>
2018-02-01 00:10:33 -0500

    Agregando estilos al main de usage

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-$(document).ready(function () {
+$(document).ready(function() {
   var $btnGithub = $('#btn-github');
 
-  $btnGithub.on('click', function (event) {
+  $btnGithub.on('click', function(event) {
     debugger;
     $(location).attr('href', 'https://github.com/Nefelijm/cardify');
   });
