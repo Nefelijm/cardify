de6d5a8070c8ff1e48418570969aaa6076c7682d
andrea-isabel <andrea19.93.15@gmail.com>
2018-02-04 16:57:47 -0500

    Agregando opciones de usuario: boxColor

--- public/js/jquery.cardify.js
+++ public/js/jquery.cardify.js
@@ -1,11 +1,15 @@
 'use strict';
 
-(function ($) {
-  $.fn.cardify = function () {
+(function($) {
+  $.fn.cardify = function(options) {
+    var settings = $.extend({
+      boxColor: '#34D1BF'
+    }, options);
+
     // Agregando estilos al contenedor
     $(this).addClass('cardifyContainer');
     // Iterar entre imágenes
-    $(this).find('img').each(function (index, image) {
+    $(this).find('img').each(function(index, image) {
       // Creando elementos
       $(image).wrap('<figure></figure>');
       $(image).parent().append($('<figcaption></figcaption>').text($(image).attr('alt')));
@@ -13,7 +17,7 @@
       // Agregando estilos a la imagen
       $(image).addClass('cardifyImage');
       // Agregando estilos al figure
-      $(image).parent().addClass('cardifyFigure');
+      $(image).parent().addClass('cardifyFigure').css('box-shadow', '#D8DAD5 8px 8px, ' + settings.boxColor + ' -8px -8px, rgba(216, 216, 216, 0.7) -5px 5px 30px 10px');
       // Agregando estilos al Figcaption
       $(image).next().addClass('cardifyFigcaption');
     });
